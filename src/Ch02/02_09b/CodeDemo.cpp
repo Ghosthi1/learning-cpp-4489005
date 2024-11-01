b938e72dec7adc5f16e029c1f349bdc6236b4c90
Callum Forsyth <47060657+Ghosthi1@users.noreply.github.com>
2024-11-01 15:40:09 +0000

    strings

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -3,9 +3,21 @@
 // Strings, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstring>
 
 int main(){
+    //making strings with characters 
+    const size_t length = 25;
+    char array_Str1[length] = "Hey guys!";
+    char array_Str2[] = "wharts up";
     
+    std::string std_str1 = "adada";
+    std::string std_str2 = "baaa";
+
+    strncat(array_Str1, array_Str2, length);
+    std::cout << array_Str1 + std_str1<< std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
