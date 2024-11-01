bbd9aa31dac835ebf3d6f645fcbb730071b8bf96
Callum Forsyth <47060657+Ghosthi1@users.noreply.github.com>
2024-11-01 15:45:39 +0000

    casting

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,22 @@
 #include <cstdint>
 
 int main(){
+
+    float flt;
+    int32_t sgn;
+    uint32_t unsgn;
+
+    //without f it is a double
+    flt = -7.66;
+    sgn = flt;
+    unsgn = sgn;
     
+    std::cout << sgn << std::endl;
+    std::cout << flt << std::endl;
+    //casts the unsigned bits into sined bits 
+    std::cout << (int32_t) unsgn << std::endl;
+
+
     std::cout << std::endl << std::endl;
     return (0);
 }
