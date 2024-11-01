6afb78018437343208493103630aae9773640ee0
Callum Forsyth <47060657+Ghosthi1@users.noreply.github.com>
2024-11-01 13:20:02 +0000

    printed variables

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -10,7 +10,13 @@ int a ,b = 5;
 int main(){
     //local variable
     bool myFlag;
+
+    a = 7;
+    myFlag = false;
+
     std::cout << "Hi There!" << std::endl;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "flag = " << myFlag << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
