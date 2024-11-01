7914c7990350431bf9008cdb16456911aa864449
Callum Forsyth <47060657+Ghosthi1@users.noreply.github.com>
2024-11-01 13:30:08 +0000

    using auto

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -6,7 +6,22 @@
 #include <typeinfo>
 
 int main(){
-    
+    auto a = 8;
+    auto b =2345678901;
+    auto c =3.14f;
+    auto d = 3.14;
+    auto e =true;
+    auto f = 'd';
+    auto g ="c++ rocks";
+
+    std::cout << "this tybe is " << typeid(a).name() << std::endl;
+    std::cout << "this tybe is " << typeid(b).name() << std::endl;
+    std::cout << "this tybe is " << typeid(c).name() << std::endl;
+    std::cout << "this tybe is " << typeid(d).name() << std::endl;
+    std::cout << "this tybe is " << typeid(e).name() << std::endl;
+    std::cout << "this tybe is " << typeid(f).name() << std::endl;
+    std::cout << "this tybe is " << typeid(g).name() << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
