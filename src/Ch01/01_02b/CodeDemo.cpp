2e27f11f5b5de9ad78871aabcd1a5532d586057e
Callum Forsyth <47060657+Ghosthi1@users.noreply.github.com>
2024-11-01 11:36:53 +0000

    Hello world

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,16 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+//shows text to screen and takes inputs
+#include <iostream>
+
+int main(){
+    //standard output
+    //then end line
+    std::cout << "Hi there!" << std::endl;
+
+    //creates 2 blank lines 
+    std::cout << std::endl << std::endl;
+    return(0);
+}
