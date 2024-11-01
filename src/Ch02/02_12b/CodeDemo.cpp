ba660a35a4844e1aa61940345c9f521378eed33d
Callum Forsyth <47060657+Ghosthi1@users.noreply.github.com>
2024-11-01 16:00:02 +0000

    enum

--- src/Ch02/02_12b/CodeDemo.cpp
+++ src/Ch02/02_12b/CodeDemo.cpp
@@ -5,14 +5,24 @@
 #include <iostream>
 #include <cstdint>
 
+//each have a int value assigned, encoding
 enum cow_purpose {dairy, meat, hide, pet};
+//STRONGLY TYPED 
+enum class ahhh {meat,cow,dairy};
 
 int main(){
     int a;
+    //be is part of class ahhh
+    ahhh b;
 
+    b = ahhh::meat;
+
+    //assigns a to the encoding of meat so 1
     a = meat;
 
     std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << (int) b << std::endl;
+
 
     std::cout << std::endl << std::endl;
     return (0);
