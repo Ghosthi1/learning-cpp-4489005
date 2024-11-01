79737e295052d0bf962fd51fec29ba51ab1d7614
Callum Forsyth <47060657+Ghosthi1@users.noreply.github.com>
2024-11-01 13:36:01 +0000

    setting size of a variable

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,16 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+#define CAPACITY 5000
 
 int main(){
     
+    //a 32 bit int 
+    int32_t large = CAPACITY;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
