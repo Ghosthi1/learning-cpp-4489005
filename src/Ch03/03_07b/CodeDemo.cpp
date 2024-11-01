5c2d4938823e275c5cc3258add3807fe90f0fb22
Callum Forsyth <47060657+Ghosthi1@users.noreply.github.com>
2024-11-01 23:51:56 +0000

    pointer object and delete

--- src/Ch03/03_07b/CodeDemo.cpp
+++ src/Ch03/03_07b/CodeDemo.cpp
@@ -7,9 +7,16 @@
 #include "cow.h"
 
 int main(){
-    cow my_cow("Hildy", 7, cow_purpose::pet);
-    std::cout << my_cow.get_name() << " is a type-" << (int) my_cow.get_purpose() << " cow." << std::endl;
-    std::cout << my_cow.get_name() << " is " << my_cow.get_age() << " years old." << std::endl;
+    cow *my_cow;
+
+    //makes a pointer
+    my_cow = new cow("Hildy", 7, cow_purpose::pet);
+    //uses -> to say its a pointer
+    std::cout << my_cow->get_name() << " is a type-" << (int) my_cow->get_purpose() << " cow." << std::endl;
+    std::cout << my_cow->get_name() << " is " << my_cow->get_age() << " years old." << std::endl;
+
+    //delete when done
+    delete my_cow;
     
     std::cout << std::endl << std::endl;
     return (0);
