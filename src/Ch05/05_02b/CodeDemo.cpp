dad6a358a2e3e0fa6ea8df54191475e445648ee0
Callum Forsyth <47060657+Ghosthi1@users.noreply.github.com>
2024-11-02 10:46:54 +0000

    Functioopns

--- src/Ch05/05_02b/CodeDemo.cpp
+++ src/Ch05/05_02b/CodeDemo.cpp
@@ -4,13 +4,37 @@
 
 #include <iostream>
 
+// typical function
+int Square(int x){
+    return x*x;
+}
+
+//using pointers 
+void Swap (int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y =temp;
+}
+
+//by refrence 
+void Swap(int& x, int&y){
+    int temp = x;
+    x=y;
+    y =temp;
+}
+
 int main(){
     int a = 9, b;
     // TODO: square
+    b = Square(a);
     std::cout << "a = " << a << ", b = " << b << std::endl;
+
     // TODO: swap
+    Swap(&a, &b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
+
     // TODO: swap
+    Swap(a,b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     
     std::cout << std::endl << std::endl;
