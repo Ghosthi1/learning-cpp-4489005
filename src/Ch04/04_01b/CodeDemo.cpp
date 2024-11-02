37c82422ae0de9838e78aea6cff1e090ccbc65f1
Callum Forsyth <47060657+Ghosthi1@users.noreply.github.com>
2024-11-02 10:08:00 +0000

    if statments

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -5,6 +5,30 @@
 #include <iostream>
 
 int main(){
+
+    int a  =1023;
+    bool flag = false;
+    char lttr = 'd';
+
+    if(a >1000){
+        std::cout << "Warning: a is o ver 1000" << std::endl;
+    }
+
+    //comaprison opreator 0 =false
+    if(a % 2){
+        std::cout << "a is odd" << std::endl;
+    }
+    else{
+        std::cout << "a is even" << std::endl;
+    }
+
+    std::cout << "The letter " << lttr<< "is ";
+    if (lttr != 'a' && lttr != 'e' && lttr != 'o'){
+        std::cout << "not ";
+    }
+    std::cout << "A vowel " << std::endl;
+
+
     
     std::cout << std::endl << std::endl;
     return (0);
